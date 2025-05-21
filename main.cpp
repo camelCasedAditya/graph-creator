b0954789413ae7a9ff9093a3683f6ad78b7efb35
U-BSD\514957 <514957@SHS-95W2NW3.beaverton.k12.or.us>
2025-05-21 13:02:48 -0700

    delete edges and algorithm started

--- main.cpp
+++ main.cpp
@@ -8,13 +8,14 @@ void addVertex(char* label, Vertex** array);
 void addEdge(char* start, char* end, int weight, Vertex** array);
 void printTable(Vertex* v);
 void deleteVertex(char* label, Vertex** array);
-
-int MAX = 20;
+void deleteEdge(char* start, char* end, Vertex** array);
+void shortestPathSetup(char* start, char* end, Vertex** array);
+void shortestPathSearch(int startIndex, int endIndex, Vertex** array, int* totals);
 
 int main() {
 
   Vertex* vertices[20];
-  for (int i = 0; i < MAX; i++) {
+  for (int i = 0; i < 20; i++) {
     vertices[i] = NULL;
   }
   
@@ -50,7 +51,7 @@ int main() {
 void addVertex(char* label, Vertex** array) {
   Vertex* v = new Vertex();
   v->setLabel(label);
-  for (int i = 0; i < MAX; i++) {
+  for (int i = 0; i < 20; i++) {
     if (array[i] == NULL) {
       array[i] = v;
       return;
@@ -66,7 +67,7 @@ void addEdge(char* start, char* end, int weight, Vertex** array) {
   int endIndex = -1;
   Vertex* endNode;
   cout << "INTO FUNCTION" << endl;
-  for (int i = 0; i < MAX; i++) {
+  for (int i = 0; i < 20; i++) {
     if (array[i] != NULL && strcmp(array[i]->getLabel(), start) == 0) {
       startIndex = i;
       startNode = array[i];
@@ -90,8 +91,8 @@ void addEdge(char* start, char* end, int weight, Vertex** array) {
 
 void printTable(Vertex* v) {
   int** array = v->getTable();
-  for (int i = 0; i < MAX+1; i++) {
-    for (int j = 0; j<MAX+1; j++) {
+  for (int i = 0; i < 21; i++) {
+    for (int j = 0; j<21; j++) {
       cout << array[i][j] << " ";
     }
     cout << "" << endl;
@@ -101,7 +102,7 @@ void printTable(Vertex* v) {
 void deleteVertex(char* label, Vertex** array) {
   Vertex* v = NULL;
   int index = -1;
-  for(int i = 0; i < MAX; i++) {
+  for(int i = 0; i < 20; i++) {
     if (array[i] != NULL && strcmp(array[i]->getLabel(), label) == 0) {
       v = array[i];
       index = i;
@@ -114,7 +115,7 @@ void deleteVertex(char* label, Vertex** array) {
     return;
   }
   int** table = v->getTable();
-  for (int j = 0; j < MAX+1; j++) {
+  for (int j = 0; j < 21; j++) {
     if (table[index+1][j] != 0) {
       cout << "in" << endl;
       Vertex* temp = array[j-1];
@@ -129,3 +130,98 @@ void deleteVertex(char* label, Vertex** array) {
   delete v;
   return;
 }
+
+void deleteEdge(char* start, char* end, Vertex** array) {
+  int startIndex = -1;
+  Vertex* startNode;
+  int endIndex = -1;
+  Vertex* endNode;
+  cout << "INTO FUNCTION" << endl;
+  for (int i = 0; i < 20; i++) {
+    if (array[i] != NULL && strcmp(array[i]->getLabel(), start) == 0) {
+      startIndex = i;
+      startNode = array[i];
+    }
+    else if (array[i] != NULL && strcmp(array[i]->getLabel(), end) == 0) {
+      endIndex = i;
+      endNode = array[i];
+    }
+  }
+
+  cout << "PASSED FOR LOOP" << endl;
+  if (startIndex == -1 || endIndex == -1) {
+    cout << "One of the labels you entered does not correspond to an existing node" << endl;
+    return;
+  }
+  else {
+    startNode->setTable(startIndex+1, endIndex+1, 0);
+    endNode->setTable(startIndex+1, endIndex+1, 0);
+  }
+}
+
+
+void shortestPathSetup(char* start, char* end, Vertex** array) {
+  int* totals = new int[20];
+  for(int a = 0; a<20; a++) {
+    totals[a] = -1;
+  }
+  int startIndex = -1;
+  Vertex* startNode;
+  int endIndex = -1;
+  Vertex* endNode;
+  cout << "INTO FUNCTION" << endl;
+  for (int i = 0; i < 20; i++) {
+    if (array[i] != NULL && strcmp(array[i]->getLabel(), start) == 0) {
+      startIndex = i;
+      startNode = array[i];
+    }
+    else if (array[i] != NULL && strcmp(array[i]->getLabel(), end) == 0) {
+      endIndex = i;
+      endNode = array[i];
+    }
+  }
+
+  cout << "PASSED FOR LOOP" << endl;
+  if (startIndex == -1 || endIndex == -1) {
+    cout << "One of the labels you entered does not correspond to an existing node" << endl;
+    return;
+  }
+  shortestPathSearch(startIndex, endIndex, array, totals);
+  return;
+}
+
+void shortestPathSearch(int startIndex, int endIndex, Vertex** array, int* totals) {
+  int* distance = totals;
+  int visited[20] = {0};
+  visited[startIndex] = 1;
+  Vertex* start = array[startIndex];
+  Vertex* end = array[endIndex];
+  
+  for(int i = 0; i < 20; i++) {
+    //if (totals[i] != -1) {
+    //int** table = array[i]->getTable();
+    //for(int j = 1; j<21; j++) {
+    //if (table[i+1][j] != 0 && (distance[j-1] > (table[i+1][j]+distance[i]))) {
+    //distance[j-1] = table[i+1][j]+distance[i];
+    //	}
+    //}
+    //}
+    int shortest = 1000000;
+    int index = -1;
+    for (int j = 0; j<20; j++) {
+      if (visited[i] == 0 && distance[i] < shortest && distance[i] != -1) {
+	shortest = distance[i];
+	index = i;
+      }
+    }
+
+    visited[index] = 1;
+    for (int k = 0; k < 20; k++) {
+
+    }
+  }
+  return;
+}
+
+
+
